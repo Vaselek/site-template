86b120af39a23d4cefffeae9e4532cc379bd9d57
Vaselek <burzhuas@gmail.com>
2018-12-13 13:56:38 +0600

    Add css and js to site-template

--- src/App.js
+++ src/App.js
@@ -2,27 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+const Header = () => {
+  return(
+      <div className="header">
+          <a href="#default" className="logo">CompanyLogo</a>
+          <div className="header-right">
+              <a className="active" href="#home">Home</a>
+              <a href="#contact">Contact</a>
+              <a href="#about">About</a>
+          </div>
       </div>
-    );
-  }
+  )
+};
+
+const Content = () => {
+  return(
+      <div className="content">
+          <h1>Responsive Header</h1>
+          <p>Resize the browser window to see the effect.</p>
+          <p>Some content..</p>
+      </div>
+  )
 }
+
+const SideBar = () => {
+  return (
+      <div className="sidebar">
+          <a href="#home"><i className="fa fa-fw fa-home"></i> Home</a>
+          <a href="#services"><i className="fa fa-fw fa-wrench"></i> Services</a>
+          <a href="#clients"><i className="fa fa-fw fa-user"></i> Clients</a>
+          <a href="#contact"><i className="fa fa-fw fa-envelope"></i> Contact</a>
+      </div>
+  )
+}
+
+const Footer = () => {
+  return (
+      <div className='footer'>
+        <p>Footer</p>
+      </div>
+  )
+}
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <SideBar />
+            <Content />
+            <Footer />
+        </div>
+    )
+};
+
+
 
 export default App;
